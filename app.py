4c8fafe222bf1fbdf99af61f12c4907abc91c94d
rubenparnell <66206505+rubenparnell@users.noreply.github.com>
2025-05-26 23:44:06 +0100

    added flask migrate

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from models import db, User
 import os
 from dotenv import load_dotenv
@@ -7,23 +8,31 @@ from routes import main as main_bp
 
 load_dotenv()
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
 login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+migrate = Migrate()
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
+    login_manager.login_view = 'login'
+
+    # Register routes
+    app.register_blueprint(main_bp)
+
+    return app
+
+app = create_app()
 
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.query(User).filter_by(id=user_id).first()
-
-# Register routes
-app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
